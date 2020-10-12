752a6fc07761a42d5652441f16c955c141b3c510
Przemysław Kowalczyk <przemek.t.kowalczyk@gmail.com>
2020-10-12 19:14:31 +0200

    Add localhost order conection

--- src/js/script.js
+++ src/js/script.js
@@ -404,6 +404,9 @@
         thisCart.dom[key] = thisCart.dom.wrapper.querySelectorAll(select.cart[key]);
       }
       thisCart.dom.form = thisCart.dom.wrapper.querySelector(select.cart.form);
+      /*phone number and adress*/
+      thisCart.dom.phone = thisCart.dom.wrapper.querySelector(select.cart.phone);
+      thisCart.dom.address = thisCart.dom.wrapper.querySelector(select.cart.address);
     }
 
     initActions() {
@@ -468,9 +471,21 @@
       const url = settings.db.url + '/' + settings.db.order;
 
       const payload = {
-        address: 'test',
+        products: [],
+        address: thisCart.dom.address,
         totalPrice: thisCart.totalPrice,
+        phone: thisCart.dom.phone,
+        totalNumber: thisCart.totalNumber,
+        subtotalPrice: thisCart.subtotalPrice,
+        deliveryFee: thisCart.deliveryFee,
       };
+      for(let product of thisCart.products){
+        let productData = product.getData();
+        payload.products += productData;
+        console.log('payloadtoggle', payload.products);
+      }
+
+      console.log('payload', payload);
 
       const options = {
         method: 'POST',
@@ -549,6 +564,20 @@
         event.preventDefault();
         thisCartProduct.remove();
       });
+    }
+
+    getData(){
+      const thisCartProduct = this;
+
+      const packedProducts = {
+        id: thisCartProduct.id,
+        amount: thisCartProduct.amount,
+        price: thisCartProduct.price,
+        priceSingle: thisCartProduct.priceSingle,
+        params: thisCartProduct.params,
+      };
+      console.log('pack', packedProducts);
+      return packedProducts;
     }
   }
 
